e114cee308c129fb0ba6656316a0799ac6c6c9c3
Rooba Murugan <rooba7308@gmail.com>
2026-08-17 20:35:49 +0530

    Time: 0 ms (100%), Space: 19.2 MB (66.84%) - LeetHub

---
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -0,0 +1,12 @@
+class Solution:
+    def inorderTraversal(self, root):
+        result = []
+
+        def inorder(node):
+            if node:
+                inorder(node.left)
+                result.append(node.val)
+                inorder(node.right)
+
+        inorder(root)
+        return result
